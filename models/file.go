3cb708ad42b065a8f8bbbd91d371d1def9a29854
daniel.henedino <daniel.henedino@telefonica.com>
2022-04-04 20:34:30 -0300

    File formatting

--- models/file.go
+++ models/file.go
@@ -1,19 +1,20 @@
 package models
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
 type File struct {
 	FileName string `json:"filename"`
 }
 
 func (i *File) Bind(r *http.Request) error {
-    if i.FileName == "" {
-        return fmt.Errorf("Filename is a mandatory field")
-    }
-    return nil
+	if i.FileName == "" {
+		return fmt.Errorf("Filename is a mandatory field")
+	}
+	return nil
 }
 func (*File) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return nil
 }
