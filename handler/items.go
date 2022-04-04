ee120995e087adb7c22b279cb4a6c2bd4dfee09f
daniel.henedino <daniel.henedino@telefonica.com>
2022-04-04 20:34:20 -0300

    Added new handlers for Items

--- handler/items.go
+++ handler/items.go
@@ -1,42 +1,120 @@
 package handler
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "strconv"
-    "github.com/go-chi/chi"
-    "github.com/go-chi/render"
-    "gitlab.com/idoko/bucketeer/db"
-    "gitlab.com/idoko/bucketeer/models"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//var itemIDKey = "itemID"
-func items(router chi.Router) {
-    router.Get("/", GetAllValidItems)          // MUST REDIRECT TO MAIN PAGE
-    router.Route("/item", func(router chi.Router) {
-        //router.Use(ItemContext)
-        router.Get("/", GetAllValidItems)
-		router.Get("/", GetAllRejectedItems)
-		router.Route("")
-        router.Put("/", updateItem)
-        router.Delete("/", deleteItem)
-    })
+// Get all valid items on database
+func getAllValidItems(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items")
+	items, err := dbInstance.GetAllValidItems()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
 }
 
+// Get all invalid (rejected) items on database
+func getAllInvalidItems(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all invalid items")
+	items, err := dbInstance.GetAllInvalidItems()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Get all items on database by cpf
+func getAllItemsByCpf(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items by CPF")
+	vars := mux.Vars(r)
+	cpf := vars["cpf"]
+	items, err := dbInstance.GetAllItemsByCpf(cpf)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Get all items on database by cpf
+func getAllItemsLastSaleCnpj(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items by last sale CNPJ")
+	vars := mux.Vars(r)
+	cnpj := vars["cnpj"]
+	items, err := dbInstance.GetAllItemsLastSale(cnpj)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Get all items on database by cpf
+func getAllItemsFrequentSaleCnpj(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items by frequent sale CNPJ")
+	vars := mux.Vars(r)
+	cnpj := vars["cnpj"]
+	items, err := dbInstance.GetAllItemsFrequentSale(cnpj)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Get all valid records count
+func getValidRecordsCount(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items count")
+	items, err := dbInstance.GetValidRecordsCount()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Get all invalid records count
+func getInvalidRecordsCount(w http.ResponseWriter, r *http.Request) {
+	log.Println("Obtaining all valid items count")
+	items, err := dbInstance.GetInvalidRecordsCount()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(items)
+	if err != nil {
+		return
+	}
+	w.Write(jsonResponse)
+}
+
+// Delete all valid items
 /*
-func ItemContext(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        itemId := chi.URLParam(r, "itemId")
-        if itemId == "" {
-            render.Render(w, r, ErrorRenderer(fmt.Errorf("item ID is required")))
-            return
-        }
-        id, err := strconv.Atoi(itemId)
-        if err != nil {
-            render.Render(w, r, ErrorRenderer(fmt.Errorf("invalid item ID")))
-        }
-        ctx := context.WithValue(r.Context(), itemIDKey, id)
-        next.ServeHTTP(w, r.WithContext(ctx))
-    })
+func deleteValidItems(w http.ResponseWriter, r *http.Request) {
+	err := dbInstance.DeleteValidItems()
+	if err != nil {
+		if err == db.ErrNoMatch {
+			render.Render(w, r, ErrNotFound)
+		} else {
+			render.Render(w, r, ServerErrorRenderer(err))
+		}
+		return
+	}
 }*/
