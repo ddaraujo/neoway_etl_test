5471f052b39c91372a66d6fac71814044ec0a0db
daniel.henedino <daniel.henedino@telefonica.com>
2022-04-04 20:34:43 -0300

    File formatting

--- models/item.go
+++ models/item.go
@@ -1,31 +1,33 @@
 package models
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
+
 type Item struct {
-	Cpf string `json:"cpf"`
-	Private string `json:"private"`
-	Incompleto string `json:"incompleto"`
-	DataUltimaCompra string `json:"data_ultima_compra"`
-	ValorTicketMedio string `json:"valor_ticket_medio"`
+	Cpf                     string `json:"cpf"`
+	Private                 string `json:"private"`
+	Incompleto              string `json:"incompleto"`
+	DataUltimaCompra        string `json:"data_ultima_compra"`
+	ValorTicketMedio        string `json:"valor_ticket_medio"`
 	ValorTicketUltimaCompra string `json:"valor_ticket_ultima_compra"`
-	CnpjLojaMaisFrequente string `json:"cpnj_loja_mais_frequente"`
-	CnpjLojaUltimaCompra string `json:"cnpj_loja_utima_compra"`
+	CnpjLojaMaisFrequente   string `json:"cpnj_loja_mais_frequente"`
+	CnpjLojaUltimaCompra    string `json:"cnpj_loja_utima_compra"`
 }
 type ItemList struct {
-    Items []Item `json:"items"`
+	Items []Item `json:"items"`
 }
+
 func (i *Item) Bind(r *http.Request) error {
-    if i.Cpf == "" {
-        return fmt.Errorf("CPF é um campo obrigatório")
-    }
-    return nil
+	if i.Cpf == "" {
+		return fmt.Errorf("CPF é um campo obrigatório")
+	}
+	return nil
 }
 func (*ItemList) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return nil
 }
 func (*Item) Render(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return nil
 }
