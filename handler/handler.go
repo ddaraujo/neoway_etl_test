eb585e0f7e55d5ca2f4e5905b219656305a76d7b
daniel.henedino <daniel.henedino@telefonica.com>
2022-04-04 20:34:05 -0300

    New routes created

--- handler/handler.go
+++ handler/handler.go
@@ -1,27 +1,65 @@
 package handler
+
 import (
-    "net/http"
-    "github.com/go-chi/chi"
-    "github.com/go-chi/render"
-    "gitlab.com/idoko/bucketeer/db"
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/ddaraujo/neoway_etl_test/db"
+
+	"github.com/gorilla/mux"
 )
+
 var dbInstance db.Database
+
 func NewHandler(db db.Database) http.Handler {
-    router := chi.NewRouter()
-    dbInstance = db
-    router.MethodNotAllowed(methodNotAllowedHandler)
-    router.NotFound(notFoundHandler)
-    //router.Route("/items", items)
-    router.Route("/upload", files)
-    return router
+	dbInstance = db
+
+	//router.MethodNotAllowed(methodNotAllowedHandler)
+	//router.NotFound(notFoundHandler)
+
+	fmt.Println("Start handling requests")
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// Landing page (index.html)
+	myRouter.HandleFunc("/", landingPage).Methods("GET")
+
+	// Upload file route (handler/files.go)
+	myRouter.HandleFunc("/upload", upload).Methods("POST")
+
+	// Valid Items Routes (handler/items.go)
+	myRouter.HandleFunc("/items/valid", getAllValidItems).Methods("GET")                                // Get all valid items
+	myRouter.HandleFunc("/items/valid/cpf/{cpf}", getAllItemsByCpf).Methods("GET")                      // Get all valid items by CPF
+	myRouter.HandleFunc("/items/valid/lastSale/{cnpj}", getAllItemsLastSaleCnpj).Methods("GET")         // Gel all valid items by cnpj (last sale)
+	myRouter.HandleFunc("/items/valid/frequentSale/{cnpj}", getAllItemsFrequentSaleCnpj).Methods("GET") // Get all valid items by cnpj (frequent sale)
+	myRouter.HandleFunc("/items/valid/count", getValidRecordsCount)                                     // Get all valid records count
+	//myRouter.HandleFunc("/items/valid/delete", deleteValidRecords)        // Delete all valid records
+
+	// Invalid Items Routes (handler/items.go)
+	myRouter.HandleFunc("/items/invalid", getAllInvalidItems).Methods("GET")
+	myRouter.HandleFunc("/items/invalid/count", getInvalidRecordsCount) // Get all invalid (rejected) records count
+	//myRouter.HandleFunc("/items/invalid/delete", deleteInvalidRecords)        // Delete all invalid records
+
+	// Database management routes
+	//myRouter.HandleFunc("/file/count", getAllValidItems)                 // Get all imported (RAW) data
+	//myRouter.HandleFunc("/file/delete", deleteImportedFile)         // Delete all imported RAW data
+
+	log.Fatal(http.ListenAndServe(":8888", myRouter))
+
+	return myRouter
 }
-func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-type", "application/json")
-    w.WriteHeader(405)
-    render.Render(w, r, ErrMethodNotAllowed)
+
+// HTML Templates
+var templates = template.Must(template.ParseFiles("index.html"))
+
+// Display HTML template
+func displayTemplate(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
 }
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-type", "application/json")
-    w.WriteHeader(400)
-    render.Render(w, r, ErrNotFound)
+
+// Show landing page (Template: index.html)
+func landingPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Show landing page")
+	displayTemplate(w, "index", nil)
 }
