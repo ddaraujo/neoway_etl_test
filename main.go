73228fef234453df97c004548671b85fc3d6e896
daniel.henedino <daniel.henedino@telefonica.com>
2022-04-06 14:35:42 -0300

    Fix retries for DB connectiong

--- main.go
+++ main.go
@@ -28,27 +28,38 @@ func main() {
 		os.Getenv("POSTGRES_DB")
 
 	// Initialize database
-	database, err := db.Initialize(dbUser, dbPassword, dbName)
-	if err != nil {
-		log.Println("Erro inicializando o database")
-		log.Fatalf("Could not set up database (error): %v", err)
+	count := 0
+	for {
+		database, err := db.Initialize(dbUser, dbPassword, dbName)
+		if err != nil {
+			if count < 5 {
+				log.Println("Error connectiong to database. Retrying...")
+				time.Sleep(5 * time.Second)
+				count++
+				continue
+			} else {
+				log.Fatalf("Could not set up database (error): %v", err)
+			}
+		}
+
+		defer database.Conn.Close()
+
+		// Http handler
+		log.Println("Starting HTTP handler")
+		httpHandler := handler.NewHandler(database)
+		server := &http.Server{
+			Handler: httpHandler,
+		}
+		defer Stop(server)
+
+		ch := make(chan os.Signal, 1)
+
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+
+		log.Println(fmt.Sprint(<-ch))
+		log.Println("Stopping API server.")
 	}
-	defer database.Conn.Close()
 
-	// Http handler
-	httpHandler := handler.NewHandler(database)
-	server := &http.Server{
-		Handler: httpHandler,
-	}
-
-	defer Stop(server)
-
-	ch := make(chan os.Signal, 1)
-
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-
-	log.Println(fmt.Sprint(<-ch))
-	log.Println("Stopping API server.")
 }
 
 func Stop(server *http.Server) {
